4a444484bd2bb901c786bdfe9eab258ecdaebb5a
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-10-24 16:10:14 +0200

    Content (inhoud)
    Content aan het overzetten, naar de nieuwe framework.

--- choices-choices-the-tech-stack-visual-thinking/app/routes/_index.tsx
+++ choices-choices-the-tech-stack-visual-thinking/app/routes/_index.tsx
@@ -1,42 +1,40 @@
 import type { MetaFunction } from "@remix-run/node";
+import { cssBundleHref } from "@remix-run/css-bundle";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Onderzoeken en begrijpen" },
+    {
+      name: "description",
+      content: "Visual thinking in het hbo: onderzoeken en begrijpen",
+    },
   ];
 };
 
-export default function Index() {
+export default function _Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 style={{ color: "red" }}>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <section>
+      <iframe
+        style={{ width: 560, height: 315, border: "none" }}
+        src="https://www.youtube.com/embed/yr-rRMElLFg?si=CkdyjnxpUFMqM7se"
+      ></iframe>
+
+      <h2 style={{ fontWeight: "bold" }}>
+        Visual thinking in het hbo: onderzoeken en begrijpen
+      </h2>
+
+      <p>
+        Deze kennisclip gaat over visual thinking en hoe het kan worden
+        toegepast om zowel studenten als docenten in het hbo op een andere
+        manier te leren onderzoeken.
+      </p>
+
+      <p>
+        Deze kennisclips zijn onderdeel van het platform ‘Visual thinking in het
+        hbo’. Dit platform staat vanaf 2024 online. Tot die tijd kunnen
+        werkvormen opgevraagd worden bij Charley Muhren, docent en onderzoeker
+        bij CMD Amsterdam: c.j.muhren@hva.nl.
+      </p>
+    </section>
   );
 }
-
