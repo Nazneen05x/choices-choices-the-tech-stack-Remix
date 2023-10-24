d82a3914b2312dece897e331be8549116560fc22
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-10-24 19:36:55 +0200

    content delete
    Ik heb de informatie weg gehaald. Deze info wil ik ophalen met een headless cms (strapi)

--- choices-choices-the-tech-stack-visual-thinking/app/routes/_index.tsx
+++ choices-choices-the-tech-stack-visual-thinking/app/routes/_index.tsx
@@ -18,23 +18,6 @@ export default function _Index() {
         style={{ width: 560, height: 315, border: "none" }}
         src="https://www.youtube.com/embed/yr-rRMElLFg?si=CkdyjnxpUFMqM7se"
       ></iframe>
-
-      <h2 style={{ fontWeight: "bold" }}>
-        Visual thinking in het hbo: onderzoeken en begrijpen
-      </h2>
-
-      <p>
-        Deze kennisclip gaat over visual thinking en hoe het kan worden
-        toegepast om zowel studenten als docenten in het hbo op een andere
-        manier te leren onderzoeken.
-      </p>
-
-      <p>
-        Deze kennisclips zijn onderdeel van het platform ‘Visual thinking in het
-        hbo’. Dit platform staat vanaf 2024 online. Tot die tijd kunnen
-        werkvormen opgevraagd worden bij Charley Muhren, docent en onderzoeker
-        bij CMD Amsterdam: c.j.muhren@hva.nl.
-      </p>
     </section>
   );
 }
