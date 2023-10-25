e861a520d8cc1dfa24a608c30331cf47505c5e5f
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-10-25 23:53:13 +0200

    Eerste opzet video!
    Ik ben echt trots!! bedankt voor de css code van mijn teamgenoten!!

--- choices-choices-the-tech-stack-visual-thinking/app/routes/_index.tsx
+++ choices-choices-the-tech-stack-visual-thinking/app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import "~/styles/index.css";
 
 export const meta: MetaFunction = () => {
   return [
@@ -18,10 +19,32 @@ export default function _Index() {
     <section>
       <Header />
 
-      <iframe
-        style={{ width: 560, height: 315, border: "none" }}
-        src="https://www.youtube.com/embed/yr-rRMElLFg?si=CkdyjnxpUFMqM7se"
-      ></iframe>
+      <article className="iframe">
+        <section className="video-arrows">
+          <button className="arrows">
+            <img
+              className="left-arrow"
+              src="arrows.svg"
+              alt="Knop met pijl naar volgende video"
+            />
+          </button>
+        </section>
+
+        <iframe
+          style={{ width: 560, height: 315, border: "none" }}
+          src="https://www.youtube.com/embed/yr-rRMElLFg?si=CkdyjnxpUFMqM7se"
+        ></iframe>
+
+        <section className="video-arrows">
+          <button className="arrows">
+            <img
+              className="right-arrow"
+              src="arrows.svg"
+              alt="Knop met pijl naar volgende video"
+            />
+          </button>
+        </section>
+      </article>
 
       <Footer />
     </section>
