f7cc4c7e828a90bb695c46615c983d5e947b92af
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-10-25 18:41:43 +0200

    Opzet footer
    Nu nog alleen styling  implementeren.

--- choices-choices-the-tech-stack-visual-thinking/app/components/Footer.tsx
+++ choices-choices-the-tech-stack-visual-thinking/app/components/Footer.tsx
@@ -3,7 +3,50 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>Dit is de footer van mijn Remix-app</p>
+      <section>
+        <img src="visual-thinking-logo-footer.svg" alt="footerlogo" />
+      </section>
+
+      <section>
+        <ul>
+          <li>Over</li>
+          <li>Blog</li>
+          <li>Tekenmethodes</li>
+        </ul>
+      </section>
+
+      <section>
+        <ul>
+          <li>Categorieën</li>
+          <li>Onderzoeken en begrijpen</li>
+          <li>Organiseren en plannen</li>
+          <li>Leren over jezelf</li>
+          <li>Leren over anderen</li>
+          <li>Communiceren en presenteren</li>
+          <li>Creatief Denken</li>
+        </ul>
+      </section>
+
+      <section>
+        <ul>
+          <li>Contact</li>
+          <li>
+            <span>
+              <img src="twitter.svg" alt="twitterlogo" />
+            </span>
+            <span>
+              <img src="instagram.svg" alt="instagramlogo" />
+            </span>
+            <span>
+              <img src="tiktok.svg" alt="tiktoklogo" />
+            </span>
+            <span>
+              <img src="youtube.svg" alt="youtubelogo" />
+            </span>
+          </li>
+          <li>© Charley Muhren, 2022</li>
+        </ul>
+      </section>
     </footer>
   );
 };
