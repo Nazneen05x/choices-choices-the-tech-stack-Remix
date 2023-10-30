689452f97b03f72cb1b2faf75d4bfcc0b2833a89
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-10-30 22:35:23 +0100

    inhoud back!!

---
+++ choices-choices-the-tech-stack-Remix/app/routes/_index.tsx
@@ -0,0 +1,50 @@
+// import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "~/styles/index.css";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Onderzoeken en begrijpen" },
+    { name: "Visual thinking in het hbo: onderzoeken en begrijpen" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <section>
+      <Header />
+
+      <article className="iframe">
+        <section className="video-arrows">
+          <button className="arrows">
+            <img
+              className="left-arrow"
+              src="arrows.svg"
+              alt="Knop met pijl naar volgende video"
+            />
+          </button>
+        </section>
+
+        <iframe
+          style={{ width: 560, height: 315, border: "none" }}
+          src="https://www.youtube.com/embed/yr-rRMElLFg?si=CkdyjnxpUFMqM7se"
+        ></iframe>
+
+        <section className="video-arrows">
+          <button className="arrows">
+            <img
+              className="right-arrow"
+              src="arrows.svg"
+              alt="Knop met pijl naar volgende video"
+            />
+          </button>
+        </section>
+      </article>
+
+      <Footer />
+    </section>
+  );
+}
